3fc3eaed5e3ceecefc08835a47c09998d6c42dfe
txema <txedav@gmail.com>
2021-11-17 14:37:01 +0100

    Working on tests

--- test/test_parsers.py
+++ test/test_parsers.py
@@ -11,14 +11,26 @@ sys.path.insert(0, parentdir)
 
 import numpy as np
 
-from parsers import (
-    _get_index_to_fit,
-    _calc_integral,
-    Decoder,
-    CoordNumber,
-    CoordNumbers,
-    Information,
-)
+# Try imports for good work of the LSP
+try:
+    from ..parsers import (
+        _get_index_to_fit,
+        _calc_integral,
+        Decoder,
+        CoordNumber,
+        CoordNumbers,
+        Information,
+    )
+except ImportError:
+    from parsers import (
+        _get_index_to_fit,
+        _calc_integral,
+        Decoder,
+        CoordNumber,
+        CoordNumbers,
+        Information,
+    )
+
 
 def test_get_index_to_fit():
     """
@@ -26,3 +38,17 @@ def test_get_index_to_fit():
     """
     arange = np.arange(100)
     assert _get_index_to_fit(arange) == 90
+
+
+def test_calc_integral():
+    """
+    Test the _calc_integral function.
+    """
+    x = np.linspace(1, 100, 1000)
+    y = -1/x**2
+    assert np.isclose(_calc_integral(x, y), -1, rtol=1e-2)
+
+def test_decoder():
+    """
+    Test the Decoder class.
+    """
